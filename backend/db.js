df82bb8933e6221ee4f02cae60c661aee047ce6a
Fedor Indutny <fedor@indutny.com>
2019-05-10 12:18:37 +0200

    backend: store in redis

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,24 @@
 'use strict';
 
-const MAX_NONCES = 1024 * 1024;
-const MAX_USERS = 1024 * 1024;
+const SECRETS = require('./secrets');
+
+const promisify = require('util').promisify;
+const redis = require('redis').createClient(SECRETS.REDIS_URL);
+
+redis.getAsync = promisify(redis.get);
+redis.setAsync = promisify(redis.set);
+redis.setexAsync = promisify(redis.setex);
+redis.delAsync = promisify(redis.del);
+redis.lpushAsync = promisify(redis.lpush);
+redis.llenAsync = promisify(redis.llen);
+redis.lrangeAsync = promisify(redis.lrange);
+redis.hsetAsync = promisify(redis.hset);
+redis.hkeysAsync = promisify(redis.hkeys);
+
+const NONCE_SIZE = 16;
+const NONCE_EXPIRATION = 300;
+const TOKEN_SIZE = 24;
+const TOKEN_EXPIRATION = 7 * 24 * 3600;
 const MAX_RESULTS = 5;
 
 const model = require('./model');
@@ -14,65 +31,68 @@ const usersById = new Map();
 const featuresByUser = new Map();
 
 exports.getNonce = async () => {
-  if (nonces.size === MAX_NONCES) {
-    const first = nonces.values().next().value;
-    nonces.delete(first);
-  }
-
-  const nonce = crypto.randomBytes(16).toString('hex');
-  nonces.add(nonce);
+  const nonce = crypto.randomBytes(NONCE_SIZE).toString('hex');
+  await redis.setexAsync('gradtype:nonce:' + nonce, NONCE_EXPIRATION, '1');
   return nonce;
 };
 
 exports.checkNonce = async (nonce) => {
-  if (!nonces.has(nonce)) {
+  if (!/^[a-f0-9]+$/.test(nonce)) {
     return false;
   }
 
-  nonces.delete(nonce);
-  return true;
+  const present = await redis.getAsync('gradtype:nonce:' + nonce);
+  await redis.getAsync('gradtype:nonce:' + nonce);
+  await redis.delAsync('gradtype:nonce:' + nonce);
+  return !!present;
 };
 
 exports.fetchUser = async (token) => {
-  return usersByToken.get(token);
+  const id = await redis.getAsync('gradtype:user-by-token:' + token);
+  if (!id) {
+    return undefined;
+  }
+  const json = await redis.getAsync('gradtype:user:' + id);
+  return JSON.parse(json);
 };
 
 exports.getToken = async (user) => {
-  if (usersByToken.size === MAX_USERS) {
-    const first = usersByToken.keys().next().value;
-    usersByToken.delete(first);
-  }
+  const token = crypto.randomBytes(TOKEN_SIZE).toString('base64');
 
-  usersById.set(user.id, user);
+  await redis.hsetAsync('gradtype:user', user.id, JSON.stringify(user));
+  await redis.setexAsync('gradtype:user-by-token:' + token,
+    TOKEN_EXPIRATION, user.id);
 
-  const token = crypto.randomBytes(24).toString('base64');
-  usersByToken.set(token, user);
   return token;
 };
 
 exports.addFeatures = async (user, features, events) => {
-  let storage;
-  if (featuresByUser.has(user.id)) {
-    storage = featuresByUser.get(user.id);
-  } else {
-    storage = { events: [], features: [] };
-    featuresByUser.set(user.id, storage);
-  }
+  await redis.lpushAsync('gradtype:events:' + user.id,
+    JSON.stringify(events));
+  const len = await redis.lpushAsync('gradtype:features:' + user.id,
+    JSON.stringify(features));
 
-  storage.events.push(events);
-  storage.features.push(features);
-
-  return { featureCount: storage.features.length };
+  return { featureCount: len };
 };
 
 exports.searchFeatures = async (features) => {
+  const users = await redis.hkeysAsync('gradtype:user');
+
   const matches = [];
-  for (const [ userId, storage ] of featuresByUser) {
-    const distance = model.computeDistance(features, storage.features);
+  await Promise.all(users.map(async (userId) => {
+    const storedLen = await redis.llenAsync('gradtype:features:' + userId);
+    let storedFeatures = await redis.lrangeAsync('gradtype:features:' + userId,
+      0, storedLen);
+
+    storedFeatures = storedFeatures.map((json) => JSON.parse(json));
+
+    const distance = model.computeDistance(features, storedFeatures);
     if (distance < 1) {
-      matches.push({ distance, user: usersById.get(userId) });
+      const json = await redis.getAsync('gradtype:user:' + userId);
+      const user = JSON.parse(json);
+      matches.push({ distance, user });
     }
-  }
+  }));
 
   matches.sort((a, b) => a.distance - b.distance);
   return matches.slice(0, MAX_RESULTS);
